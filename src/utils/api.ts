53339921b63f1974f253ebc2bc55b148bdfb33d9
mohammadGhiasvand <m.ghiasvand.work@gmail.com>
2023-09-08 00:08:39 +0200

    heroku VPS is used here

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,3 @@
-export const BASE_API_URL: string = `https://survey-mern-clone.onrender.com/api`;
-export const BASE_BACKEND_URL: string = `https://survey-mern-clone.onrender.com`;
-export const BASE_API_IMAGE_url: string = `https://survey-mern-clone.onrender.com`;
+export const BASE_API_URL: string = `https://survey-io-a55d0a4b5941.herokuapp.com/api`;
+export const BASE_BACKEND_URL: string = `https://survey-io-a55d0a4b5941.herokuapp.com`;
+export const BASE_API_IMAGE_url: string = `https://survey-io-a55d0a4b5941.herokuapp.com`;
