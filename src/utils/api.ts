bc5a0972342de6dd004d57a43a112361718d9463
mohammadGhiasvand <m.ghiasvand.work@gmail.com>
2023-09-07 17:31:12 +0200

    Final changes are here!

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,3 @@
-const PORT = '5000';
-
 export const BASE_API_URL: string = `https://survey-mern-clone.onrender.com/api`;
 export const BASE_BACKEND_URL: string = `https://survey-mern-clone.onrender.com`;
 export const BASE_API_IMAGE_url: string = `https://survey-mern-clone.onrender.com`;
